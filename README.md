e3e89df72dd93f8e30955468329bc5cf25d2c66c
kimimberlyyen <work223305@gmail.com>
2023-02-21 10:07:38 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# YOUTUBE
+
+[Follow 3:10](https://www.youtube.com/watch?v=Law7wfdg_ls)
+
+---
+
+Page Name
+Shop = Site
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './About.css'
+
+function About() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  )
+}
+
+export default About

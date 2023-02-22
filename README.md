c4fa495bc76241ff9f222ac3f3209c5fea9d2d7e
Kimberly <work223305@gmail.com>
2023-02-22 21:42:29 +0800

    Router Routes Route Outlet Done

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # YOUTUBE
 
-[Follow 3:10](https://www.youtube.com/watch?v=Law7wfdg_ls)
+[Follow 3:10-版本是 2019 已經不合用](https://www.youtube.com/watch?v=Law7wfdg_ls)
 [Figma by Eric](https://www.figma.com/file/kENLKGH76xcovjPI1h4mjq/Side-Project?t=jbWp4NUyv4b2IVsA-0)
+[work223305@gmail 簡報+切版判斷](https://docs.google.com/presentation/d/1fAE6Bh9IHxIoQjzUwHF0Q6zRWOBvEqKA6vEG6Ss9cZQ/edit?usp=sharing)
 
 ---
 
--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,32 @@ import "./App.css";
 import Nav from "./Nav";
 import About from "./About";
 import Team from "./Team";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Team />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          element={
+            <div className="App">
+              <Nav />
+              <Outlet />
+              <Team />
+            </div>
+          }
+        >
+          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
